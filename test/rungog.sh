97e7ac45ecb6663960b9b38968c87848a5e460a8
Yifan Gu <guyifan1121@gmail.com>
2014-11-30 22:44:06 -0500

    Replace GOPATH

--- test/rungog.sh
+++ test/rungog.sh
@@ -2,7 +2,7 @@
 
 ipaddr=$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
 
-rootpath=$GOPATH/src/github.com/go-distributed/gog
+rootpath=/root/gopher/src/github.com/go-distributed/gog
 logpath=$rootpath/test/log
 
 $rootpath/gog -addr=$ipaddr:8424 > $logpath/stdout 2>$logpath/stderr
