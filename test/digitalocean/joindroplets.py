72b4de818ceaa42d147e3cce958e2cc4c4b08eca
Yifan Gu <guyifan1121@gmail.com>
2014-12-02 16:51:30 -0500

    do bootstrap

--- test/digitalocean/joindroplets.py
+++ test/digitalocean/joindroplets.py
@@ -6,17 +6,21 @@ import json
 
 token = ""
 specialname = "gogs"
-num = 40
+num = 50
+nodeaddr = ""
 
 def main():
-    if len(sys.argv) != 3:
-        print "usage: %s [token] [num_per_nodes]" %sys.argv[0]
+    if len(sys.argv) != 4:
+        print "usage: %s [token] [num_per_nodes] [node_addr]" %sys.argv[0]
         return -1
 
     global token
     global num
+    global nodeaddr
+
     token = sys.argv[1]
     num = int(sys.argv[2])
+    nodeaddr = sys.argv[3]
 
     nullf = open("/dev/null", "w")
 
@@ -28,6 +32,7 @@ def main():
 
     jdroplets =  json.loads(droplets)["droplets"]
 
+    total = 0
     for i in range(0, len(jdroplets)):
         dropid = jdroplets[i]["id"]
         name = jdroplets[i]["name"]
@@ -36,8 +41,9 @@ def main():
             continue
         for j in range(0, num):
             restaddr = dropip+":"+str(9000+j)
-            print "joining %d, addr %s" % (dropid, restaddr)
-            subprocess.call(["curl", "http://"+restaddr+"/api/join", "-d", "peer=104.236.9.169:8000"])
+            print "joining %d, addr %s" % (total, restaddr)
+            total = total + 1
+            subprocess.call(["curl", "http://"+restaddr+"/api/join", "-d", "peer="+nodeaddr+":8000"])
 
 if __name__ == '__main__':
     main()
